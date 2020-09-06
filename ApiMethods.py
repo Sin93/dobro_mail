26669e265c1c9a899079a9607d5828273af81834
Maxim <zinovev.maxim93@mail.ru>
2020-09-07 01:08:48 +0300

    Добавил класс для работы с API и класс для работы с логикой бота, пока всё на костылях, но постарался сделать подробное описание каждого костыля, чтоб было понятно что дальше делать

---
+++ ApiMethods.py
@@ -0,0 +1,67 @@
+from config import TOKEN
+from vk_api import VkApi, utils
+from typing import Optional
+
+import json
+
+
+class ApiMethodsClass():
+    def __init__(self, user_id, message, keyboard):
+        self.user_id = user_id
+        self.message = message
+        self.keyboard = keyboard
+
+        self.json_keyboard = self.create_json_keyboard(self.keyboard)
+
+    def create_json_keyboard(self, keyboard, inline=False):
+        """Принимает список кнопок, и где их располагать (inline=True - в сообщении, =False - в клавиатуре внизу)
+        генерирует из них словарь, а затем переводит в json.
+        Возвращает строку с json объектом."""
+        keyboard_for_send = {
+            "one_time": False,
+            "buttons": []
+        }
+
+        if inline:
+            keyboard_for_send['inline'] = True
+
+        for button in keyboard:
+            new_button = [{
+                "action": {label
+                    "type": button['text'],
+                    "label": button['label']
+                },
+                "color": button['color']
+            }]
+
+            if payload:
+                new_button[0]['action']['payload'] = payload
+
+            keyboard_for_send["buttons"].append(new_button)
+
+        return json.dumps(keyboard_for_send)
+
+
+    @staticmethod
+    def send_msg_and_keyboard(
+        user_id: int,
+        message: str,
+        keyboard: Optional[str]=None):
+
+        vk_session = VkApi(token=TOKEN)
+        vk = vk_session.get_api()
+
+        random_id = utils.get_random_id()
+        if keyboard is not None:
+            vk.messages.send(
+                user_id=user_id,
+                message=message,
+                keyboard=keyboard,
+                random_id=random_id
+            )
+        else:
+            vk.messages.send(
+                user_id=user_id,
+                message=message,
+                random_id=random_id
+            )
--- startbot.py
+++ startbot.py
@@ -1,27 +1,22 @@
 from config import TOKEN, VK_GROUP_ID
 from vk_api import VkApi
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
-
-import requests
-import vk_api
+from BotLogic import Bot
 
 
-def write_msg(user_id, message):
-    random_id = vk_api.utils.get_random_id()
-    vk.messages.send(
-        user_id=user_id,
-        message=message,
-        random_id=random_id
-    )
+def main():
+    vk_session = VkApi(token=TOKEN)
+    vk = vk_session.get_api()
+    longpoll = VkBotLongPoll(vk_session, VK_GROUP_ID)
 
+    for event in longpoll.listen():
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            user_id = event.object.message['from_id']
+            user_message = event.object.message['text']
+            information = event.object.message['text']
 
-vk_session = VkApi(token=TOKEN)
-vk = vk_session.get_api()
-longpoll = VkBotLongPoll(vk_session, VK_GROUP_ID)
+            # создаётся эктемпляр класса, который управляет логикой бота
+            received_message = Bot(user_id, user_message, information)
 
-for event in longpoll.listen():
-    if event.type == VkBotEventType.MESSAGE_NEW:
-        from_user_id = event.object.message['from_id']
-        print(event.object)
-
-        write_msg(from_user_id, 'Ответ бота')
+if __name__ == '__main__':
+    main()
