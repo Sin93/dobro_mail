ef66adcfbaf7b5bbda7606d209021d493c07fe8d
KurskiySergey <59256173+KurskiySergey@users.noreply.github.com>
2020-09-10 23:40:51 +0300

    bot_2.0
    Дописал дополнительные методы для бота. Немного переписал API метод в inite под свой вывод данных Протестировал в группе. Пока что отзывается на команды из списка возможных проектов. Типа природа, пожилые и т.д. На прочее просит повторить запрос. Выводит списки проектов с кнопками подробнее и пожертвовать.
    Над остальным пока в процессе

---
+++ chat_Bot_2.0/startbot.py
@@ -0,0 +1,22 @@
+from config import TOKEN, VK_GROUP_ID
+from vk_api import VkApi
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from user_request import Bot
+
+
+def main():
+    vk_session = VkApi(token=TOKEN)
+    vk = vk_session.get_api()
+    longpoll = VkBotLongPoll(vk_session, VK_GROUP_ID)
+
+    for event in longpoll.listen():
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            user_id = event.object.message['from_id']
+            user_message = event.object.message['text']
+            information = event.object.message['text']
+
+            # создаётся эктемпляр класса, который управляет логикой бота
+            Bot(user_id, user_message, information)
+
+if __name__ == '__main__':
+    main()
